696fe304e79b78d21681ab705659c78bd2c49674
wmoai <wmoai@mail.goo.ne.jp>
2014-02-28 19:31:09 +0900

    fix reserve and record

---
+++ epgdump.js
@@ -0,0 +1,85 @@
+var fs = require('fs')
+  , exec = require('child_process').exec
+  , model = require('./model')
+;
+
+var chs = [
+  27,
+  25,
+  22,
+  21,
+  24,
+  23,
+  16
+];
+
+function saveEpg(epg, ch) {
+  var programs = epg[0].programs;
+  var service = new model.service;
+  var sid = epg[0].service_id;
+  service.id = sid;
+  service.name = epg[0].name;
+  service.save(function(err) {
+  });
+
+  for(var i=0; i<programs.length; i++) {
+    var data = programs[i];
+    var program = new model.program;
+    program.sid = sid;
+    program.phch = ch;
+    program.eid = data.event_id;
+    program.title = data.title;
+    program.detail = data.detail;
+    program.start = data.start / 10;
+    program.end = data.end / 10;
+    program.duration = data.duration * 1000;
+    if (data.category.length > 0) {
+      program.categoryL = data.category[0].large.ja_JP;
+      program.categoryM = data.category[0].middle.ja_JP;
+    }
+    program.save(function(err) {
+      // if (err) { console.log(err); }
+    });
+  }
+}
+
+var epgDumper = {
+  _getEpg : function() {
+    self = this;
+    if (self.channels.length <= self.index) {
+      return;
+    }
+    var ch = self.channels[self.index];
+    var tsName = 'tmp/tmp'+ch+'.ts';
+    var cmdRec = 'recpt1 --b25 --strip '+ch+' 20 '+tsName;
+    console.log("rec start "+ch);
+    exec(cmdRec, {timeout: 30000}, function(error, stdout, stderr) {
+      var epgName = 'tmp/tmp'+ch+'.json';
+      var cmdDump = 'epgdump json '+tsName+' '+epgName;
+      console.log("dump start "+ch);
+      exec(cmdDump, {timeout: 2000}, function(error, stdout, stderr) {
+        var cmdCat = 'cat '+epgName;
+        exec(cmdCat, {timeout: 2000}, function(error, stdout, stderr) {
+          var epg = JSON.parse(stdout);
+          saveEpg(epg, ch);
+          self.index++;
+          self._getEpg();
+          console.log("end "+ch);
+          fs.unlink(tsName);
+          fs.unlink(epgName);
+        });
+      })
+    });
+  },
+  get : function(channels) {
+    this.index = 0;
+    this.channels = channels;
+    this._getEpg();
+  }
+}
+
+module.exports = function() {
+  console.log('start epgdump');
+  epgDumper.get(chs);
+};
+
--- schedule.js
+++ schedule.js
@@ -1,48 +1,79 @@
 var cronJob = require('cron').CronJob
   , mongoose = require('mongoose')
   , model = require('./model')
+  , exec = require('child_process').exec
+  , epgdump = require('./epgdump')
 ;
 
-// mongoose.connect('mongodb://localhost/moareco');
+mongoose.connect('mongodb://localhost/moareco');
 
 var Program = model.program;
+var reserved = [];
 
 var checkReserveJob = new cronJob({
   // cronTime: "0 */30 * * * *"
   cronTime: "*/5 * * * * *",
   onTick: function() {
-    // Program
-    // .find({reserved:true, start:{'$lt':1390644010000}}, function(err, docs) {
-      // console.log(docs);
-    // });
+    var now = new Date().getTime();
+    var there = now + 2 * 3600 * 1000;
+    Program
+    .find({
+      reserved:true,
+      start:{
+        '$gt':now,
+        '$lt':there
+      }
+    })
+    .exec(function(err, docs) {
+      for(var i=0; i<docs.length; i++) {
+        var program = docs[i];
+        if (typeof(reserved[program.eid]) == 'undefined') {
+          // reserve program
+          console.log('reserve '+program.eid);
+          reserved[program.eid] = new ReserveJob(program);
+        }
+      }
+    });
   },
   onComplete: function() {
-
   },
   start: true,
   timeZone: "Asia/Tokyo"
 });
 
 
-
 // start recording
-var ReserveJob = function() {
-  this.eid = 60;
+var ReserveJob = function(program) {
+  this.program = program;
   this.job = new cronJob({
-
+    cronTime: new Date(program.start),
+    onTick: function() {
+      // record start
+      console.log(program.eid);
+      var tsName = program.eid+'.ts';
+      var duration = program.duration / 1000;
+      var cmdRec = 'recpt1 --b25 --strip '+program.phch+' '+duration+' '+tsName;
+      console.log('start recording '+program.eid);
+      exec(cmdRec, {timeout: duration+60000}, function(error, stdout, stderr) {
+        console.log('end recording '+program.eid);
+      });
+    },
+    onComplete: function() {
+      delete reserved[program.eid];
+    },
+    start: true,
+    timeZone: "Asia/Tokyo"
   });
 }
 
 
-// update epg everyday
+// update epg everyday 6 o'clock
 var updateEpgJob = new cronJob({
-  // cronTime: "0 0 6 * * *"
-  cronTime: "*/5 * * * * *",
+  cronTime: "0 0 6 * * *",
   onTick: function() {
-    console.log(new Date());
+    epgdump();
   },
   onComplete: function() {
-
   },
   start: true,
   timeZone: "Asia/Tokyo"
